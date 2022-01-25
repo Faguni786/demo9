758112f074f393f7a12465b2b64e69765ef6e53f
Faguni <70260827+Faguni786@users.noreply.github.com>
2022-01-25 06:28:05 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+
+var trex ,trex_running,edges;
+function preload(){
+trex_running=loadAnimation("trex1.png","trex3.png","trex4.png")
+
+}
+
+function setup(){
+  createCanvas(600,200)
+  
+  //create a trex sprite
+ trex = createSprite(50,160,20,50)
+ trex.addAnimation("running",trex_running)
+ edges=createEdgeSprites()
+
+
+ trex.scale = 0.5
+ trex.x =50
+}
+
+function draw(){
+  background("cyan")
+ console.log(trex.y)
+  if (keyDown("space")){
+    trex.velocityY = -10
+  }
+
+  trex.velocityY= trex.velocityY + 0.5;
+  trex.collide(edges[3])
+
+
+
+  drawSprites()
+
+}
